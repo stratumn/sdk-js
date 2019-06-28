62325feafcccc42e37820c9e9f2c34ce4194cd68
Jeremie Dayan <jeremie@stratumn.com>
2019-06-28 17:45:50 +0200

    with sdk

--- src/types/sdk.ts
+++ src/types/sdk.ts
@@ -1,3 +1,4 @@
+import { sig } from '@stratumn/js-crypto';
 import { ClientConfig } from './client';
 
 /**
@@ -8,4 +9,39 @@ export interface SdkConfig extends ClientConfig {
    * The workflow id (instantiate one sdk per workflow)
    */
   workflowId: string;
+}
+
+/**
+ * Setup interface for the sdk
+ */
+export interface SdkSetup {
+  /**
+   * The workflow id
+   */
+  workflowId: string;
+
+  /**
+   * The user id
+   */
+  userId: string;
+
+  /**
+   * The account id
+   */
+  accountId: string;
+
+  /**
+   * The group id
+   */
+  groupId: string;
+
+  /**
+   * The owner id
+   */
+  ownerId: string;
+
+  /**
+   * The private key used for signing links
+   */
+  signingPrivateKey: sig.SigningPrivateKey;
 }
