4c0e81cdb18589c5c0abd9f8b85e72b30ec19752
Jeremie Dayan <jeremie@stratumn.com>
2019-07-22 11:34:44 +0200

    export TraceLink and TraceLinkBuilder

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './fileWrapper';
 export * from './fileRecord';
 export { SdkOptions } from './types/sdk';
 export * from './types/trace';
+export * from './traceLink';
+export * from './traceLinkBuilder';
