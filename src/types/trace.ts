cc95ea98a872aee3001015bbf6bfcc07833c9875
haroldcb <haroldcb@hotmail.fr>
2021-04-12 16:09:12 +0200

    Fix formatting [[BTA-912]

--- src/types/trace.ts
+++ src/types/trace.ts
@@ -166,13 +166,7 @@ export interface PaginationResult {
  * of a workflow.
  */
 export interface SearchTracesFilter {
-  tags:
-    | {
-        overlaps: string[];
-      }
-    | {
-        contains: string[];
-      };
+  tags: { overlaps: string[] } | { contains: string[] };
 }
 
 /**
